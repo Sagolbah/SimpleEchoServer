d15990fecb0930837f0b81f1860ee6095d10c83f
Daniil Boger <daniil.boger@jetbrains.com>
2025-03-26 17:04:33 +0200

    add dependency, attempt 3

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,6 +2,7 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -25,13 +26,13 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2025.03"
+version = "2024.12"
 
 project {
     description = "echo server project"
 
     // Define Build Configuration
-    buildType {
+    val echoServerBuild = buildType {
         id("EchoServerBuild")
         name = "Build Echo Server"
 
@@ -81,5 +82,65 @@ project {
         artifactRules = """
             target/*.jar => artifacts
         """.trimIndent()
+    }
+
+    // Define second build configuration with snapshot dependency
+    buildType {
+        id("EchoServerDeploy")
+        name = "Deploy Echo Server"
+
+        // VCS Settings
+        vcs {
+            root(DslContext.settingsRoot)
+        }
+
+        // Build Steps
+        steps {
+            // Clean and compile
+            maven {
+                name = "Clean and Compile"
+                goals = "clean compile"
+                runnerArgs = "-Dmaven.test.failure.ignore=true"
+            }
+
+            // Run tests
+            maven {
+                name = "Run Tests"
+                goals = "test"
+                runnerArgs = "-Dmaven.test.failure.ignore=true"
+            }
+
+            // Package application
+            maven {
+                name = "Package Application"
+                goals = "package"
+                runnerArgs = "-Dmaven.test.skip=true"
+            }
+        }
+
+        // Triggers
+        triggers {
+            vcs {
+                branchFilter = "+:*"
+            }
+        }
+
+        // Features
+        features {
+            perfmon {
+            }
+        }
+
+        // Artifacts
+        artifactRules = """
+            target/*.jar => artifacts
+        """.trimIndent()
+
+        // Add snapshot dependency on the first build
+        dependencies {
+            snapshot(echoServerBuild) {
+                onDependencyFailure = FailureAction.FAIL_TO_START
+            }
+        }
     }
 }
